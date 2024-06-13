f9d354a209bf9b00e58feb30a7fb7abd72973a59
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-06-13 08:26:26 +0500

    add new file

--- obj/app.js
+++ obj/app.js
@@ -1,2 +1,17 @@
+var user1 = { name: "user", age: 50, admin: false }; // obj constructor
 var user1 = { name: "user", age: 50, admin: false };
-var user1 = { name: "user", age: 50, admin: false };
+
+function user(name, age, admin) {
+  this.UserName = name;
+  this.age = age;
+  this.admin = admin;
+}
+
+let users = new user("Sattor", 10, true);
+let users1 = new user("Bolta", 50, false);
+
+console.log(users, users1);
+
+function target(event) {
+  console.log(event.target.tagName);
+}
