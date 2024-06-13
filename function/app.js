7c3bc7f74f0e16900398aba563f7138c2ef78b35
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-06-13 08:38:10 +0500

    function add

---
+++ function/app.js
@@ -0,0 +1,19 @@
+function person(name) {
+  console.log(`hello ${name}`);
+}
+person("Hasanboy");
+// function  declaration
+
+var person1 = function (name) {
+  console.log(`hello ${name}`);
+};
+
+person1("ey");
+
+// function  expretion
+
+const hello = (name) => {
+  console.log(`hello ${name}`);
+};
+
+hello("tim");
