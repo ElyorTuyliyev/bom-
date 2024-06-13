b94176e82b169dde3f8185a0cf70e307903bd40e
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-06-13 10:40:44 +0500

    Prototype add

--- function/app.js
+++ function/app.js
@@ -1,19 +1,36 @@
-function person(name) {
-  console.log(`hello ${name}`);
-}
-person("Hasanboy");
-// function  declaration
+// function person(name) {
+//   console.log(`hello ${name}`);
+// }
+// person("Hasanboy");
+// // function  declaration
 
-var person1 = function (name) {
-  console.log(`hello ${name}`);
+// var person1 = function (name) {
+//   console.log(`hello ${name}`);
+// };
+
+// person1("ey");
+
+// function  expration
+
+// const hello = (name) => {
+//   console.log(`hello ${name}`);
+// };
+
+// hello("tim");
+
+// const obj = {
+//   name: "Elyor",
+//   age: 10,
+//   city: "jizzax",
+//   info: () => {
+//     console.log(this);
+//   },
+// };
+
+// obj.info();
+
+const myAge = (age = 10, name = "gamma") => {
+  console.log(name + age);
 };
 
-person1("ey");
-
-// function  expretion
-
-const hello = (name) => {
-  console.log(`hello ${name}`);
-};
-
-hello("tim");
+myAge("betta", 100);
