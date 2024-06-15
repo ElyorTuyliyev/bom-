d87aab71b38339aba64fecb665798c8e56cc51a7
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-06-15 13:30:07 +0500

    add new file

--- localStroy/app.js
+++ localStroy/app.js
@@ -1,12 +1,13 @@
 let imgsWrapper = document.getElementById("imgs__wrapper");
 let link = document.getElementById("imgLink");
 let imgGet = JSON.parse(localStorage.getItem("link"));
-let arr = [];
+console.log(imgGet);
+
 function imgs() {
   localStorage.setItem("link", JSON.stringify(link.value));
 }
 
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < imgGet.length; i++) {
   imgsWrapper.innerHTML += `<img class = 'imgs' src="${imgGet}" alt="">
   `;
 }
